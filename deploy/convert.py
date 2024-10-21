6bce8e1451bea714fe57eab7f54eb899862975f8
Kiprin Sergei <sergei@jinteki.dev>
2024-10-21 17:43:29 +0500

    added rename operation

--- deploy/convert.py
+++ deploy/convert.py
@@ -1,11 +1,28 @@
 import sys
 from label_studio_sdk.converter.imports.yolo import convert_yolo_to_ls
+from pathlib import Path
+from uuid import uuid4
 
 if __name__ == "__main__":
-    convert_yolo_to_ls(
-        ".data/" + sys.argv[1],
-        ".data/" + sys.argv[2],
-        image_root_url=f"/data/local-files/?d={sys.argv[1]}/images",
+    operation = sys.argv[1]
+    pack = sys.argv[2]
+
+    datadir = Path(".data") / pack
+
+    if operation == "rename":
+        images = (datadir / "images").glob("*.JPG")
+        labels = (datadir / "labels").glob("*.txt")
+        for image in images:
+            old_name = image.stem.split("-")[1]
+            image.rename(image.parent / f"{old_name}{image.suffix}")
+        for label in labels:
+            old_name = label.stem.split("-")[1]
+            label.rename(label.parent / f"{old_name}{image.suffix}")
+    elif operation == "convert":
+        convert_yolo_to_ls(
+        str(datadir),
+        str(datadir / "output.json"),
+        image_root_url=f"/data/local-files/?d={pack}/images",
         image_ext=".JPG",
     )
 
